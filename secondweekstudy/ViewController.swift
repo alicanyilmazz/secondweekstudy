f8c9dc96723a4850dfe551f8a214bc9e12b02a4f
alicanyilmazz <alicanyilmaz101@gmail.com>
2021-09-11 10:31:02 +0300

    Fibonacci method added.

--- secondweekstudy/ViewController.swift
+++ secondweekstudy/ViewController.swift
@@ -12,8 +12,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(calculateFibonacci())
     }
 
+   // This function calculated first 1000 fibonacci number but we only return bigger 750 ones and function upperbound is 1000.
+   func calculateFibonacci() -> [Double]{
+        var fibonacciLength : Double = 1000 , leftNumber : Double = 0 , rightNumber : Double = 1 , nextNumber : Double = 0
+        var myArr : [Double] = [Double]()
 
+        while fibonacciLength > 2 {
+            nextNumber = leftNumber + rightNumber
+            leftNumber = rightNumber
+            rightNumber = nextNumber
+            if rightNumber > 750 {
+                myArr.append(rightNumber)
+            }
+            fibonacciLength -= 1
+        }
+        return myArr
+    }
 }
 
